e34df82615c64badd99a1095d52df62fe528c315
Pedro Levandowski <146142910+Pedro-Levandowski@users.noreply.github.com>
2025-11-24 20:25:22 -0300

    :tada: feat: Configuracao inicial do projeto com Spring Boot

---
+++ src/main/java/com/grupo6/barbearia_api/BarbeariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.barbearia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo6/barbearia_api/BarbeariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.barbearia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
