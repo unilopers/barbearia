3d6d466e622724bd44c6db46261acec8c2a437dd
Pedro Levandowski <146142910+Pedro-Levandowski@users.noreply.github.com>
2025-11-25 00:13:04 -0300

    :sparkles: feat: Adiciona view de Funcionarios

---
+++ src/main/java/com/grupo6/barbearia_api/view/FuncionarioView.java
@@ -0,0 +1,8 @@
+package com.grupo6.barbearia_api.view;
+
+import com.grupo6.barbearia_api.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioView extends JpaRepository<Funcionario, Long> { }
