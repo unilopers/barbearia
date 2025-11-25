b3f4333a6c92f1f26cb61781573abd17013a0df9
Pedro Levandowski <146142910+Pedro-Levandowski@users.noreply.github.com>
2025-11-25 10:45:10 -0300

    :bug: fix: Adiciona getters e setters ao dto Agendamento

--- src/main/java/com/grupo6/barbearia_api/dto/AgendamentoRequestDTO.java
+++ src/main/java/com/grupo6/barbearia_api/dto/AgendamentoRequestDTO.java
@@ -1,15 +1,53 @@
 package com.grupo6.barbearia_api.dto;
 
-import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
 public class AgendamentoRequestDTO {
     private Long clienteId;
     private Long funcionarioId;
     private List<Long> servicosIds;
     private LocalDateTime dataHora;
     private String observacoes;
+
+    public Long getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Long clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public Long getFuncionarioId() {
+        return funcionarioId;
+    }
+
+    public void setFuncionarioId(Long funcionarioId) {
+        this.funcionarioId = funcionarioId;
+    }
+
+    public List<Long> getServicosIds() {
+        return servicosIds;
+    }
+
+    public void setServicosIds(List<Long> servicosIds) {
+        this.servicosIds = servicosIds;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
 }
 
