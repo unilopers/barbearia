bf05dbc1f58b3af77b6a1c9c3611d7318e4ccdb1
EnzoFenato <enzofenato@edu.unifil.br>
2025-11-25 00:28:37 -0300

    Commit Entidade Cliente

---
+++ src/main/java/com/grupo6/barbearia_api/model/Cliente.java
@@ -0,0 +1,30 @@
+package com.grupo6.barbearia_api.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "cliente")
+@Data
+
+public class Cliente {
+    @Id
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String nome;
+    @Column(nullable = false, lenght = 20)
+    private String telefone;
+    @Column(unique = true, lenght = 14)
+    private String cpf;
+    @Column (name = "data_nascimento")
+    private LocalDate data_Nascimento;
+    @Lob
+    private String observacoes;
+    @Column (nullable = false)
+    private boolean ativo = true;
+    @Column (name = "data_cadastro",
+    nullable = false, updatable = false)
+    private LocalDateTime dataCadastro = LocalDateTime.now();
+
+
+}
