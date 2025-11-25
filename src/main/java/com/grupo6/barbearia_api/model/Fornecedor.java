21abf65ed08d917206c3742a7287078d53e98edb
Marcos Vinicios <viniciusbolo03@gmail.com>
2025-11-25 11:59:33 -0300

    :pencil2: fix(model): corrigir package, parênteses e erros de digitação no model Fornecedor

--- src/main/java/com/grupo6/barbearia_api/model/Fornecedor.java
+++ src/main/java/com/grupo6/barbearia_api/model/Fornecedor.java
@@ -1,4 +1,4 @@
-package com.grupo6.barbeariaapi.model;
+package com.grupo6.barbearia_api.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "fornecedor")
 @Data
-public class fornecedor {
+public class Fornecedor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,15 +24,15 @@ public class fornecedor {
     @Column(length = 100)
     private String email;
 
-    @Column)length = 225)
+    @Column (length = 225)
     private String endereço;
 
-    @lomb
+    @Lob
     private String observacoes;
 
     @Column(nullable = false)
     private boolean ativo = true;
 
     @Column(name = "data_cadastro", nullable = false, updatable = false)
-    private LocalDataTime dataCadastro = LocalDataTime.now();
+    private LocalDateTime dataCadastro = LocalDateTime.now();
 }
