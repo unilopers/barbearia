5731447b6867cf7502bc64b4bf5d33a9e8d26ed3
Marcos Vinicios <viniciusbolo03@gmail.com>
2025-11-25 15:39:31 -0300

    :bug: fix: correção de código

--- src/main/java/com/grupo6/barbearia_api/controller/FornecedorController.java
+++ src/main/java/com/grupo6/barbearia_api/controller/FornecedorController.java
@@ -1,7 +1,7 @@
 package com.grupo6.barbearia_api.controller;
 
 import com.grupo6.barbearia_api.model.Fornecedor;
-import com.grupo6.barbearia_api.repository.FornecedorRepository;
+import com.grupo6.barbearia_api.view.FornecedorView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +14,16 @@ import java.util.List;
 public class FornecedorController {
 
     @Autowired
-    private FornecedorRepository fornecedorRepository;
+    private FornecedorView fornecedorView;
 
     @GetMapping
     public List <Fornecedor> listar() {
-        return fornecedorRepository.findAll();
+        return fornecedorView.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Fornecedor> buscarPorID(@PathVariable Long id) {
-        return fornecedorRepository.findById(id)
+        return fornecedorView.findById(id)
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.notFound().build());
     }
@@ -31,24 +31,24 @@ public class FornecedorController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Fornecedor criar(@RequestBody Fornecedor fornecedor) {
-        return fornecedorRepository.save(fornecedor);
+        return fornecedorView.save(fornecedor);
     }
 
     @PutMapping(";{id}")
     public ResponseEntity<Fornecedor> atualizar(@PathVariable Long id, @RequestBody Fornecedor fornecedor) {
-        if (!fornecedorRepository.existsById(id)) {
+        if (!fornecedorView.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
         fornecedor.setId(id);
-        return ResponseEntity.ok(fornecedorRepository.save(fornecedor));
+        return ResponseEntity.ok(fornecedorView.save(fornecedor));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
-        if (!fornecedorRepository.existsById(id)) {
+        if (!fornecedorView.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
-        fornecedorRepository.deleteById(id);
+        fornecedorView.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
