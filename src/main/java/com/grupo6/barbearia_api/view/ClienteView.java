609dbeb9d045689c59cad4e99358d7e72fa411db
EnzoFenato <enzofenato@edu.unifil.br>
2025-11-25 10:33:45 -0300

    :sparkles: feat: Adiciona ClienteView ao Projeto

--- src/main/java/com/grupo6/barbearia_api/view/ClienteView.java
+++ src/main/java/com/grupo6/barbearia_api/view/ClienteView.java
@@ -2,6 +2,7 @@ package com.grupo6.barbearia_api.view;
 import com.grupo6.barbearia_api.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
 
 public interface ClienteView extends JpaRepository<Cliente, Long> {
