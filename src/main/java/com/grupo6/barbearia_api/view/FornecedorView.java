2d2fd7d08148ccae1a0c5138e274475d0fce4e8c
Marcos Vinicios <viniciusbolo03@gmail.com>
2025-11-25 15:49:05 -0300

    :sparkles: feat: Adicionar View Fornecedor

--- src/main/java/com/grupo6/barbearia_api/view/FornecedorView.java
+++ src/main/java/com/grupo6/barbearia_api/view/FornecedorView.java
@@ -2,7 +2,7 @@ package com.grupo6.barbearia_api.view;
 
 import com.grupo6.barbearia_api.model.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.View;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FornecedorView extends JpaRepository<Fornecedor, Long> {
