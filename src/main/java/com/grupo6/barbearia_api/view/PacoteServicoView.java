01d1bff475187a560e11f1b5f3e18d0bd9f76ec8
Pedro Levandowski <146142910+Pedro-Levandowski@users.noreply.github.com>
2025-11-25 09:40:21 -0300

    :sparkles: feat: Cria view PacoteServico

---
+++ src/main/java/com/grupo6/barbearia_api/view/PacoteServicoView.java
@@ -0,0 +1,9 @@
+package com.grupo6.barbearia_api.view;
+
+import com.grupo6.barbearia_api.model.PacoteServico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacoteServicoView extends JpaRepository<PacoteServico, Long> {
+}
