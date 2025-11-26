86df04209eed8a03303be0b8a8b40bbbfc078ca5
Lucas José Gomes Oliveira <lucas.jose@edu.unifil.br>
2025-11-25 22:38:12 -0300

    :sparkles: Criação da feature ProdutoController com operações CRUD

--- src/main/java/com/grupo6/barbearia_api/controller/ProdutoController.java
+++ src/main/java/com/grupo6/barbearia_api/controller/ProdutoController.java
@@ -1,6 +1,6 @@
 package br.com.unilopers.barbearia_api.controller;
 import br.com.unilopers.barbearia_api.model.Produto;
-import br.com.unilopers.barbearia_api.repository.ProdutoRepository;
+import br.com.unilopers.barbearia_api.repository.ProdutoView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +10,15 @@ import java.util.List;
 @RequestMapping("/produtos")
 public class ProdutoController {
     @Autowired
-    private ProdutoRepository produtoRepository;
+    private ProdutoView produtoView;
     @GetMapping
     public List <Produto> listar() {
-        return produtoRepository.findAll();
+        return produtoView.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Produto> buscarPorId(@PathVariable Long id) {
-        return produtoRepository.findById(id)
+        return produtoView.findById(id)
         .map(ResponseEntity::ok)
         .orElse(ResponseEntity.notFound().build());
     }
@@ -26,26 +26,26 @@ public class ProdutoController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Produto criar(@RequestBody Produto produto) {
-        return produtoRepository.save(produto);
+        return produtoView.save(produto);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Produto> atualizar(@PathVariable Long id, @RequestBody Produto produto) {
-        if (!produtoRepository.existsById(id)) {
+        if (!produtoView.existsById(id)) {
     return ResponseEntity.notFound().build();
     }
 
     produto.setId(id);
-        return ResponseEntity.ok(produtoRepository.save(produto));
+        return ResponseEntity.ok(produtoView.save(produto));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
-        if(!produtoRepository.existsById(id)) {
+        if(!produtoView.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
 
-        produtoRepository.deleteById(id);
+        produtoView.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
