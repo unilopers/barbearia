d1bba02ab6ef92da16b2060d14ef6a7f343709c2
Lucas José Gomes Oliveira <lucas.jose@edu.unifil.br>
2025-11-25 22:29:35 -0300

    :sparkles: Adição da feature ProdutoView

--- src/main/java/com/grupo6/barbearia_api/view/ProdutoView.java
+++ src/main/java/com/grupo6/barbearia_api/view/ProdutoView.java
@@ -1,1 +1,7 @@
+package br.com.unilopers.barbearia_api.view;
 
+import br.com.unilopers.barbearia_api.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ProdutoView extends JpaRepository<Produto, Long> {}
